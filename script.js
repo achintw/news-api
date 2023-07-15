4e6f4e8cc228b548660a257a2a77f7c49b6ca6a2
Achintya Tiwary <121821415+achintw@users.noreply.github.com>
2023-07-15 08:36:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "a5790010d0914b75bda0b90d2b58bdd6";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
